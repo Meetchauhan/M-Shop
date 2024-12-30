ceae348e11f5353571a6d1a81419cac58952a033
Meet Chauhan <35761989+Meetchauhan@users.noreply.github.com>
2024-12-30 14:01:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(
   cors({
     origin: "https://m-shop-nu.vercel.app/api",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
