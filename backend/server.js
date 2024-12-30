ed9dd454660973d84542f974a323c9ccc73d0c01
Meet Chauhan <35761989+Meetchauhan@users.noreply.github.com>
2024-12-30 14:14:44 +0530

    Update server.js2

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 8000;
 // Set up CORS middleware
 app.use(
   cors({
-  origin: ["http://localhost:5000", "https://m-shop-gq4d.vercel.app"],
+  origin: ["https://m-shop-gq4d.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
