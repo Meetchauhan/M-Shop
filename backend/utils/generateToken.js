c285490e95c4ca2f95b55f6003f882ab87cd601c
Meet Chauhan <35761989+Meetchauhan@users.noreply.github.com>
2025-01-01 18:12:26 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,6 @@ export const generateToken = (res, userId) => {
     httpOnly: true,
     secure: true,
     sameSite: "None",
-    domain: "m-shop-2.onrender.com",
     maxAge: 24 * 60 * 60 * 1000,
   });
 };
